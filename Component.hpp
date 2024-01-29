be4d4111ab435e38b4a618217db37b5df48d56cc
marc2k3 <marc2003@live.co.uk>
2024-01-29 10:18:23 +0000

    Update Component.hpp

--- Component.hpp
+++ Component.hpp
@@ -2,11 +2,12 @@
 
 namespace Component
 {
-	namespace urls
-	{
-		static constexpr wil::zwstring_view docs = L"https://jscript-panel.github.io/docs/";
-		static constexpr wil::zwstring_view releases = L"https://github.com/jscript-panel/release/releases";
-	}
+	static const D2D1::ColorF White = D2D1::ColorF(D2D1::ColorF::White);
+	static constexpr auto ErrorBackground = RGB(225, 60, 45);
+	static constexpr wil::zwstring_view ErrorText = L"JavaScript error";
+
+	static constexpr wil::zwstring_view Docs = L"https://jscript-panel.github.io/docs/";
+	static constexpr wil::zwstring_view Releases = L"https://github.com/jscript-panel/release/releases";
 
 	std::string get_resource_text(int id);
 	std::wstring get_path();
@@ -14,7 +15,6 @@ namespace Component
 
 	static const std::string default_user_agent = fmt::format("{}/{}", dll, version_string);
 	static constexpr wil::zwstring_view default_font_name = L"Segoe UI";
-	static constexpr wil::zwstring_view error_text = L"JavaScript error";
 	static constexpr uint32_t max_image_size = 64 * 1024 * 1024;
 
 	extern cfg_string pin_to;
