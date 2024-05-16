51ce618025f7f7d76bb299f775edd393692d042f
marc2k3 <marc2003@live.co.uk>
2024-05-16 23:09:30 +0100

    Remove advconfig / MetadbIndex init from Component

--- Component.cpp
+++ Component.cpp
@@ -16,39 +16,16 @@ namespace Component
 	cfgDialogPosition dialog_position(guids::dialog_position);
 	cfg_string pin_to(guids::cfg_string_pin_to, "");
 
-	namespace
+	DECLARE_COMPONENT_VERSION(name, version_string, about)
+
+	class InstallationValidator : public component_installation_validator
 	{
-		DECLARE_COMPONENT_VERSION(name, version_string, about)
-
-		class InstallationValidator : public component_installation_validator
+	public:
+		bool is_installed_correctly() final
 		{
-		public:
-			bool is_installed_correctly() final
-			{
-				return test_my_name(dll.data()) && IsWindows10OrGreater() && core_version_info_v2::get()->test_version(2, 1, 0, 0);
-			}
-		};
-
-		FB2K_SERVICE_FACTORY(InstallationValidator);
-
-		advconfig_branch_factory advconfig_branch(name.data(), guids::advconfig_branch, advconfig_branch::guid_branch_tools, 0.0);
-		advconfig_string_factory advconfig_pin_to("Playback Statistics Title Format", guids::advconfig_pin_to, guids::advconfig_branch, 0.0, "$lower($meta(artist,0) - %title%)", preferences_state::needs_restart);
-
-		void init_stage()
-		{
-			string8 str;
-			advconfig_pin_to.get(str);
-
-			if (str != pin_to)
-			{
-				// If pattern has changed, nuke old data before GUID is registered
-				pin_to = str;
-				PlaybackStatistics::api()->erase_orphaned_data(guids::metadb_index);
-			}
-
-			MetadbIndex::init();
+			return test_my_name(dll.data()) && IsWindows10OrGreater() && core_version_info_v2::get()->test_version(2, 1, 0, 0);
 		}
+	};
 
-		FB2K_ON_INIT_STAGE(init_stage, init_stages::after_config_read)
-	}
+	FB2K_SERVICE_FACTORY(InstallationValidator);
 }
