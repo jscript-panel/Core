5474082e6b0b8df44d567122fcdaedcf4f4e2ee4
marc2k3 <marc2003@live.co.uk>
2024-09-08 11:44:45 +0100

    3.7.4

--- Component.hpp
+++ Component.hpp
@@ -9,8 +9,8 @@ namespace Component
 	static constexpr std::string_view name = "JScript Panel 3";
 	static constexpr std::string_view dll = "foo_jscript_panel3.dll";
 
-	static constexpr uint32_t version_number = 30703;
-	static constexpr std::string_view version_string = "3.7.3";
+	static constexpr uint32_t version_number = 30704;
+	static constexpr std::string_view version_string = "3.7.4";
 	static const std::string name_version = fmt::format("{}{}", name, version_string.substr(1));
 	static const std::string default_user_agent = fmt::format("{}/{}", dll, version_string);
 
