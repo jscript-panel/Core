00a1a66ae132908f41293b29739ab8c7e9db3074
marc2k3 <marc2003@live.co.uk>
2024-10-07 22:31:53 +0100

    Update ScriptHost

--- ScriptHost.hpp
+++ ScriptHost.hpp
@@ -25,12 +25,13 @@ public:
 	void Reset();
 
 	std::string m_build_string, m_name;
-	std::unordered_map<CallbackID, DISPID> m_callback_map;
 
 private:
 	HRESULT InitCallbackMap();
+	HRESULT InitScriptEngine();
+	HRESULT InitVersion();
 	HRESULT ParseImports();
-	HRESULT ParseScript(std::string_view code, std::string_view path);
+	HRESULT ParseScript(std::string_view path);
 	std::optional<DISPID> GetDISPID(CallbackID id);
 	std::string ExtractValue(std::string_view str);
 	std::string GetErrorText(IActiveScriptError* err);
@@ -41,6 +42,7 @@ private:
 	PanelBase* m_panel;
 	SCRIPTSTATE m_state{};
 	Strings m_imports;
+	std::unordered_map<CallbackID, DISPID> m_callback_map;
 	std::unordered_map<DWORD, std::string> m_context_to_path_map;
 	wil::com_ptr_t<IActiveScript> m_script_engine;
 	wil::com_ptr_t<IActiveScriptProperty> m_script_property;
